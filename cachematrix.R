1092c7262ecbd86670da1f1a2f9ff57b40b0ef15
aussiemum <rosiejones_au@yahoo.com>
2015-02-19 17:11:07 -0500

    added a comment to check github submission
    added a comment to check github submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,9 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+# submitting to check use of github
+# Last modified February 19th, 5:07pm EST
+
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
